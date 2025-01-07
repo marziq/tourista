af96347118538e86476973aa0cb56a5fcd3b692c
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2025-01-07 12:02:00 +0800

    add image column to migration table

--- database/migrations/2025_01_03_095448_create_hotels_table.php
+++ database/migrations/2025_01_03_095448_create_hotels_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('location');
             $table->float('price', 2);
             $table->boolean('availability');
+            $table->string('image')->nullable();
             $table->timestamps();// Created and updated timestamps
         });
     }
--- database/migrations/2025_01_03_095456_create_rentals_table.php
+++ database/migrations/2025_01_03_095456_create_rentals_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('brand');
             $table->float('price', 2);
             $table->boolean('available');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
