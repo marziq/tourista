ec604684a2755023e767663bc3bd24e9ab899fc6
Amiraazhari <nuramira.azhari@live.iium.edu.my>
2025-01-09 21:38:56 +0800

    update migration & model flight

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'departure',
+        'arrival',
+        'departure_time',
+        'arrival_time',
+        'price',
+        'airline',
+        'image',
+    ];
 }
