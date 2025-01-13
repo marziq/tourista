b3f8873d1ff8240dd144c18f4dcfe204addaa7cf
Amiraazhari <nuramira.azhari@live.iium.edu.my>
2025-01-13 14:22:39 +0800

    payment

--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -1,78 +1,70 @@
 <?php
 
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Flight;
+use Illuminate\Support\Facades\DB;
 
 class FlightSeeder extends Seeder
 {
     public function run()
     {
-        Flight::create([
-            'departure' => 'Kuala Lumpur (KUL)',
-            'arrival' => 'Penang (PEN)',
-            'travel_date' => '2025-01-15',
-            'passenger_count' => 150,
-            'price' => 120.00,
-            'airline' => 'Malaysia Airlines',
-        ]);
-
-        Flight::create([
-            'departure' => 'Kuala Lumpur (KUL)',
-            'arrival' => 'Kota Kinabalu (BKI)',
-            'travel_date' => '2025-01-20',
-            'passenger_count' => 120,
-            'price' => 250.00,
-            'airline' => 'AirAsia',
-        ]);
-
-        Flight::create([
-            'departure' => 'Penang (PEN)',
-            'arrival' => 'Langkawi (LGK)',
-            'travel_date' => '2025-01-25',
-            'passenger_count' => 100,
-            'price' => 80.00,
-            'airline' => 'Firefly',
-        ]);
-
-        Flight::create([
-            'departure' => 'Johor Bahru (JHB)',
-            'arrival' => 'Kuching (KCH)',
-            'travel_date' => '2025-01-18',
-            'passenger_count' => 180,
-            'price' => 300.00,
-            'airline' => 'Malaysia Airlines',
-        ]);
-
-        Flight::create([
-            'departure' => 'Kuching (KCH)',
-            'arrival' => 'Miri (MYY)',
-            'travel_date' => '2025-02-10',
-            'passenger_count' => 200,
-            'price' => 150.00,
-            'airline' => 'AirAsia',
-        ]);
-
-        Flight::create([
-            'departure' => 'Kuala Lumpur (KUL)',
-            'arrival' => 'Terengganu (TGG)',
-            'travel_date' => '2025-03-01',
-            'passenger_count' => 180,
-            'price' => 100.00,
-            'airline' => 'Malindo Air',
-        ]);
-
-        Flight::create([
-            'departure' => 'Kota Kinabalu (BKI)',
-            'arrival' => 'Sandakan (SDK)',
-            'travel_date' => '2025-03-15',
-            'passenger_count' => 220,
-            'price' => 200.00,
-            'airline' => 'MASwings',
+        DB::table('flights')->insert([
+            [
+                'departure' => 'Kuala Lumpur',
+                'arrival' => 'Penang',
+                'travel_date' => '2025-04-10',
+                'passenger_count' => 1,
+                'price' => '150.00',
+                'airline' => 'Malaysia Airlines',
+                'image' => '/images/MasAL.PNG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'departure' => 'Johor Bahru',
+                'arrival' => 'Kota Kinabalu',
+                'travel_date' => '2025-05-15',
+                'passenger_count' => 2,
+                'price' => '200.00',
+                'airline' => 'AirAsia',
+                'image' => '/images/Airasia.PNG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'departure' => 'Kuching',
+                'arrival' => 'Langkawi',
+                'travel_date' => '2025-06-20',
+                'passenger_count' => 3,
+                'price' => '250.00',
+                'airline' => 'Malindo Air',
+                'image' => '/images/malindo.PNG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'departure' => 'Ipoh',
+                'arrival' => 'Malacca',
+                'travel_date' => '2025-07-25',
+                'passenger_count' => 4,
+                'price' => '180.00',
+                'airline' => 'Firefly',
+                'image' => '/images/firefly.PNG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'departure' => 'Kota Bharu',
+                'arrival' => 'Alor Setar',
+                'travel_date' => '2025-08-30',
+                'passenger_count' => 5,
+                'price' => '170.00',
+                'airline' => 'Malaysia Airlines',
+                'image' => '/images/MasAL.PNG',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
-
-
